97dc6620e90502a10392b080baec5713dc2b924a
Sizana Portal <suzana.a.portal@gmail.com>
2021-11-24 15:55:25 -0300

    Exercício 6 completo.

---
+++ exercicio06/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Exercicio02
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número que corresponde ao dia da semana de 1 a 7");
+            int diaDaSemana = Convert.ToInt16(Console.ReadLine());
+            switch (diaDaSemana)
+            {
+                case 1:
+                    Console.WriteLine("Domingo");
+                    break;
+                case 2:
+                    Console.WriteLine("Segunda-feira");
+                    break;
+                case 3:
+                    Console.WriteLine("Terça-feira");
+                    break;
+                case 4:
+                    Console.WriteLine("Quarta-feira");
+                    break;
+                case 5:
+                    Console.WriteLine("Quinta-feira");
+                    break;
+                case 6:
+                Console.WriteLine("Sexta-feira");
+                    break;
+                case 7:
+                    Console.WriteLine("Sábado");
+                    break;
+                default:
+                Console.WriteLine("Dia da semana digitado inválido.");
+                break;
+            }
+        }
+    }
+}
