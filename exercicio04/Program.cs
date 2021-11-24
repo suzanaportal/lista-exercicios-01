2c473436dba5b02de94bafe012d49f656bd4b7d8
Sizana Portal <suzana.a.portal@gmail.com>
2021-11-24 14:24:16 -0300

    Meus exercícios do 01 ao 04 completos.

---
+++ exercicio04/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exercicio02
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+Console.WriteLine("Por favor, informe seu salário.");
+string? salario = Console.ReadLine();
+double salarioOriginal = Convert.ToDouble(salario);
+double percentualReajuste = 0;
+double novoSalario = 0;
+double valorAumento = 0;
+if (salarioOriginal < 2800)
+{
+    percentualReajuste = 0.2;
+}
+else if (salarioOriginal >= 2800 && salarioOriginal < 7000)
+{
+    percentualReajuste = 0.15;
+}
+else if (salarioOriginal >= 7000 && salarioOriginal < 15000)
+{
+percentualReajuste = 0.1;
+}
+else 
+{
+    percentualReajuste = 0.05;
+}
+valorAumento  = percentualReajuste*salarioOriginal;
+novoSalario = valorAumento+salarioOriginal;
+Console.WriteLine($"O salário original é {salarioOriginal}. O percentual de reajuste foi de  {percentualReajuste*100}. O valor real do aumento foi de {valorAumento}. O novo salário é de {novoSalario}.");
+        }
+    }
+}
---
+++ exercício 03/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercicio03
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Por favor, digite o valor do primeiro produto.");
+            string? valorProduto01 = Console.ReadLine();
+            Console.WriteLine("Por favor, agora, digite o valor do 2º produto.");
+            string? valorProduto02 = Console.ReadLine();
+            Console.WriteLine("Por favor, digite o valor do 3º produto.");
+            string? valorProduto03 = Console.ReadLine();
+            double produto01 = Convert.ToDouble(valorProduto01);
+            double produto02 = Convert.ToDouble(valorProduto02);
+            double produto03 = Convert.ToDouble(valorProduto03);
+            if (produto01 < produto02 && produto01 < produto03)
+            {
+                Console.WriteLine("Escolha o produto 01");
+            }
+else if (produto02 < produto03 && produto02 < produto03)
+            {
+                Console.WriteLine("Escolha produto 02");
+            }
+            else
+            {
+                Console.WriteLine("Escolha produto 03");
+            }
+        }
+    }
+}
