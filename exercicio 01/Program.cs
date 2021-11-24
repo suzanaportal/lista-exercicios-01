a90977bcdde4798fe4f1989b909e2cc6a4042f3a
Sizana Portal <suzana.a.portal@gmail.com>
2021-11-24 09:44:47 -0300

    Meu primeiro commit com os exercícios 01 completo e 02 ainda incompleto.

---
+++ exercicio 01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercicio01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+Console.WriteLine("Por favor, digite a 1ª nota.");
+string? textoNota01 = Console.ReadLine();
+Console.WriteLine("Por favor, digite a 2ª nota.");
+string? textoNota02 = Console.ReadLine();
+double nota01 = Convert.ToDouble(textoNota01);
+double nota02 = Convert.ToDouble(textoNota02);
+double media = (nota01 + nota02) /2;
+if (media >=     7) 
+{
+    Console.WriteLine("Aprovado com sucesso! Parabéns!");
+}
+else if (media <7)
+{
+Console.WriteLine("Infelizmente você não atingiu a média necessária.");
+}
+else
+Console.WriteLine("Você não digitou os dados corretamente.");
+        }
+    }
+}
---
+++ exercicio 02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exercicio02
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+Console.WriteLine("Por favor, digite o primeiro número.");
+string? textoNumero01 = Console.ReadLine();
+Console.WriteLine("Por favor digite o 2º número.");
+string? textoNumero02 = Console.ReadLine();
+Console.WriteLine("Por favor, digite o 3º número.");
+string? textoNumero03 = Console.ReadLine();
+int numero01 = Convert.ToInt32(textoNumero01);
+int numero02 = Convert.ToInt32(textoNumero02);
+int numero03 = Convert.ToInt32(textoNumero03);
+int maior = 0;
+if (numero01 > numero02) 
+{
+    if (numero01 > numero03)
+    {
+        maior = mumero01;
+    }
+    else
+    {
+        maior= numero03;
+    }
+    else
+}
+
+        }
+    }
+}
